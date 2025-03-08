6ff8305f774264c1900d0ed4e599336bfa441d94
jesusfortea <464186.joan23@fje.edu>
2025-03-08 14:36:52 +0100

    Realización del móvil

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="./stylesheets/stylesheet_index.css">
+</head>
+<body>
+    
+    <section class="container_mobile">
+        <div class="box_up">    
+            <span class="camera"></span>
+            <span class="circles"></span>
+            <span class="audio_up"></span>
+        </div>
+        <div class="screen">
+
+            <button class="screen_message">Nuevo Mensaje</button>
+
+        </div>
+        <div class="buttons_down">
+            <span class="btn_home"></span>
+        </div>
+    </section>
+
+</body>
+</html>
